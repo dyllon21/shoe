c6c333ba74a3c3cee39027e3b22ba966e53c2160
Dyllon <isaacsdyllon123@gmail.com>
2017-10-11 11:29:08 +0200

    initial commit

--- shoe_catalogue.js
+++ shoe_catalogue.js
@@ -3,7 +3,7 @@
 //function to show all shoes:
 function showAllShoes() {
   $.ajax({
-    url: 'https://localhost:5000/api/shoes',
+    url: 'http://localhost:5000/api/shoes',
     type: 'GET'
   }).done(function(results) {
     //compiling handlebars templates:
@@ -23,7 +23,7 @@ function shoeBrandAndSize() {
   var selectedBrand = document.getElementById('brandSelect').value;
 
   $.ajax({
-    url: 'https//localhost:5000/api/shoes/brand/' + selectedBrand + '/size/' + selectedSize,
+    url: 'http//localhost:5000/api/shoes/brand/' + selectedBrand + '/size/' + selectedSize,
     type: 'GET'
   }).done(function(results) {
     //compiling handlebars templates:
@@ -41,7 +41,7 @@ function shoeBrand() {
   var selectedBrand = document.getElementById('brandSelect').value;
 
   $.ajax({
-    url: "https://localhost:5000/api/shoes/brand/" + selectedBrand,
+    url: "http://localhost:5000/api/shoes/brand/" + selectedBrand,
     type: "GET"
   }).done(function(results) {
     //compiling handlebars templates:
@@ -57,9 +57,8 @@ function shoeBrand() {
 
 function shoeSize() {
   var selectedSize = document.getElementById('sizeSelect').value;
-
   $.ajax({
-    url: "https://localhost:5000/api/shoes/size/" + selectedSize,
+    url: "http://localhost:5000/api/shoes/size/" + selectedSize,
     type: "GET"
   }).done(function(results) {
     //compiling handlebars templates:
@@ -83,7 +82,7 @@ $('.submit').on('click', function() {
         InStock: document.querySelector('#inStock').value
       }
       $.ajax({
-        url: 'https://localhost:5000/api/shoes/',
+        url: 'http://localhost:5000/api/shoes/',
         type: 'POST',
         data: shoes,
         success: function(err, result) {
